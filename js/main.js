0d6ea1e29ccef4bc4d5c5c1dbb3e775355f5e83e
angel2101 <angel_alexito123@hotmail.com>
2020-02-05 13:17:23 -0600

    primer commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+'use strict'
+
+$(document).ready(function(){
+
+//slider
+    $(function(){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: false,
+            slideWidth: 1200,
+            responsive: true
+        });
+      });
+
+//post
+var posts = [
+    {
+        tittle: 'pruba de título 1',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+    {
+        tittle: 'pruba de título 2',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+    {
+        tittle: 'pruba de título 3',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+    {
+        tittle: 'pruba de título 4',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+    {
+        tittle: 'pruba de título 5',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+    {
+        tittle: 'pruba de título 6',
+        date: 'Publicado el dia ' + moment().format('DD') + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY'),
+        content: 'Etiam sagittis sem purus, ut efficitur lorem placerat ac. Praesent posuere, elit et pharetra bibendum, ipsum diam laoreet sem, non volutpat tortor orci quis lacus. Sed sit amet bibendum purus. Phasellus aliquet nunc et dictum bibendum. Nunc et pulvinar ante, eget interdum nulla. Proin iaculis porttitor elit, et consequat est consequat quis. Nulla at diam dolor. Curabitur leo urna, mattis ac massa nec, sagittis interdum velit. Proin sit amet turpis gravida, rhoncus neque vel, fringilla sapien. Donec nibh ex, lacinia in lorem eget, tempus auctor nisl. Proin congue dolor mi, sit amet mattis turpis sodales a. Aliquam rutrum urna mattis, malesuada nunc eu, convallis nisl. Aenean vestibulum eros augue, non ultrices sapien aliquet id. Phasellus fermentum cursus eros, non pharetra risus placerat nec.'
+    },
+];
+
+    posts.forEach((item, index)=>{
+        var post = `
+            <article class="post">
+            <h2>${item.tittle}</h2>
+                <span class="date">${item.date}</span>
+                <p>
+                    ${item.content}     
+                </p>
+                <a href="#" class="button-more">Leer más</a>
+            </article>
+        `
+        $('#posts').append(post);
+    }); 
+
+//selector de tema
+    var theme = $('#theme');
+
+    $('#to-green').click(function(){
+        theme.attr('href','css/green.css'); 
+    });
+
+    $('#to-red').click(function(){
+        theme.attr('href','css/red.css'); 
+    });
+
+    $('#to-blue').click(function(){
+        theme.attr('href','css/blue.css'); 
+    });
+//scroll arriba de la we
+
+    $('.subir').click(function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+
+        return false;
+    });
+});
